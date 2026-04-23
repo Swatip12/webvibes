3319468456f79d04f6cc2ab4217d8cb344d8c960
Swatip12 <paliwalswati603@gmail.com>
2026-04-23 12:29:05 +0530

    ksdksj

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SeoService } from '../../services/seo.service';
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
